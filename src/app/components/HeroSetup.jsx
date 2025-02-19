faab3961ab4da75c219f7750044da40da7c59b3a
GuiHeinrichs <heinrichsgui@gmail.com>
2025-02-18 22:26:41 -0300

    Feat: adding spin loading component into main file to turn it into global scope of the project

--- src/app/components/HeroSetup.jsx
+++ src/app/components/HeroSetup.jsx
@@ -1,16 +1,23 @@
 'use client';
 import React, { useState } from 'react';
+
+//icons
+import { GiCook } from "react-icons/gi";
+
+//components
+import { useRouter } from 'next/navigation';
+import { TagsInput } from './TagSelect';
+import Header from './Header';
+import useRecipeStore from '../stores/recipeDataStore';
 import CheckboxCards from './CheckboxCards';
 import RestrictionsCards from './RestrictionsCards';
-import useRecipeStore from '../stores/recipeDataStore';
-import { useRouter } from 'next/navigation';
-import { GiCook } from "react-icons/gi";
-import { TagsInput } from './TagSelect';
+
+//libs
 import { motion } from "framer-motion";
-import Header from './Header';
 
 export default function HeroSetup() {
     const router = useRouter();
+    
     const [ingredients, setIngredients] = useState([]);
     const [preferences, setPreferences] = useState({
         'vegetariano': false,
@@ -21,33 +28,35 @@ export default function HeroSetup() {
         'sem-gluten': false,
         'sem-lactose': false
     }]);
-
+    
     const { setRecipeData } = useRecipeStore();
-
+    
     const handleChangePreferences = (data) => {
         setPreferences(data);
     };
-
+    
     const handleChangeIngredients = (data) => {
         setIngredients(data);
     };
-
+    
     const handleChangeRestrictions = (data) => {
         setRestrictions(data);
     };
-
+    
     const handleSearch = () => {
         const recipeData = { ingredients, preferences, restrictions };
         setRecipeData(recipeData); // Salva os dados na store
         router.push("/culinaryChat");
     };
-
+    
     const anyValueEntered = () => {
         return ingredients.length > 0 ||
-            preferences['vegetariano'] || preferences['vegano'] || preferences['sem-opcoes'] ||
-            restrictions['sem-gluten'] || restrictions['sem-lactose'];
+        preferences['vegetariano'] || preferences['vegano'] || preferences['sem-opcoes'] ||
+        restrictions['sem-gluten'] || restrictions['sem-lactose'];
     };
-
+    
+    leapfrog.register();
+    
     return (
         <div className='flex flex-col items-center justify-center space-y-8 md:px-10 bg-background'>
             <Header ShowBurger={true} />
